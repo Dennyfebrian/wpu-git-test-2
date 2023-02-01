ba634b8bafc61cf63d9397f417bcdfaf84a63acb
Dennyfebrian <dennyfebrian168@gmail.com>
2023-02-01 10:45:11 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>coba git</title>
+</head>
+<body>
+    <h1>hello world</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta sint facere unde! Reiciendis, ullam. In iste esse earum voluptates. Quas nobis quos illum impedit similique repellat voluptas pariatur animi corporis.</p>
+</body>
+</html>
